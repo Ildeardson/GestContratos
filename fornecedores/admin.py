69e7b11fc53b726a1b4c4042b3bd8a27205de325
Ildeardson <ildeardson@gmail.com>
2021-06-16 19:56:26 -0300

    Iniciada classe Forncedores

--- fornecedores/admin.py
+++ fornecedores/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Fornecedores
 
 # Register your models here.
+class FornecedorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Fornecedores, FornecedorAdmin)
