095a35e21a4bdfdfbbf1ccc52823f08233a038b5
Ildeardson <ildeardson@gmail.com>
2021-06-19 17:04:42 -0300

    atulizado repositorio

--- produtos/tests.py
+++ produtos/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Produtos
+
+
+class ProdtosModelTests(TestCase):
+
+    def setUp(self):
+        self.prod1 = Produtos()
+        self.prod1.descricao = "arroz"
+        self.prod1.un = 'un'
+        self.prod1.quantidade = 10 
+        self.prod1.valor = 50.00
+
+    def test_fildes_models(self):
+        self.assertEqual(self.prod1.descricao, "arroz")
+        self.assertEqual(self.prod1.valor, 50.00)
+        
+    def test_funç_v_total(self):
+        self.assertEqual(self.prod1.v_total(), 500.00)
+
+
